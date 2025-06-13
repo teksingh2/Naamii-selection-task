c82ab358cfc8d5f21a34659642be19d159298689
teksingh2 <ayertek225@gmail.com>
2025-06-13 15:02:31 +0545

    remove-unecessary-files

--- Task3/test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-regions = ['tibia', 'femur', 'background']
-# --- Configuration ---
-# Path to your saved pickle file
-features_filepath = '/Users/teksinghayer/Desktop/Naamii-selection-task/Task3/extracted_features.pkl'
-with open(features_filepath, 'rb') as f:
-    temp_features = pickle.load(f)
-    print("Example layer keys:", temp_features)
